8ccedc0e6512cfbd083fe0738794c30eadd90565
gopala <gopal1852000@gmail.com>
2025-10-21 20:57:00 +0530

    winston setup done

---
+++ app.js
@@ -0,0 +1,22 @@
+import winston from "winston";
+
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.prettyPrint(),
+
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+    ]
+});
+
+logger.error("error severe")
+logger.warn("this is a warn")
+logger.silly("silly me")
+logger.info("today is diwali")
+
