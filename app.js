61982bd00d8621ce351317cc9de4c833e81314c4
gopala <gopal1852000@gmail.com>
2025-10-21 21:25:11 +0530

    server added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import winston from "winston";
 
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
     level: "info",
     format: winston.format.combine(
         winston.format.timestamp(),
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { logger } from './app.js'
+const app = express();
+
+app.get("/health", (req, res)=>{
+    res.json({
+        msg: "hih there"
+    });
+    logger.warn("Health point")
+
+    return;
+});
+
+app.get('/cpu', (req, res)=>{
+    const sum = 0;
+    for(let i = 0; i < 100000; i++){
+        sum += i;
+    }
+
+    res.json({
+        sum,
+    });
+    logger.error("Cpu hit high");
+    return;
+
+
+})
+
+
+app.listen(3000, ()=>{
+    logger.info("Server running");
+})
